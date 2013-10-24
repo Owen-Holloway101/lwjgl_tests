663f40274759c76d1f24d3e305bb9d4a058d98a2
Owen Holloway <owen.holloway101@gmail.com>
2013-10-24 22:30:01 +1100

    Playing with controls

--- tk/zeryter/snoozebox/render/Render.java
+++ tk/zeryter/snoozebox/render/Render.java
@@ -5,7 +5,8 @@ import org.lwjgl.opengl.*;
 
 public class Render extends RenderMain {
 
-    float rotateX = 45f, rotateY =45f, rotateZ = 0f;
+    float rotateX = 0f, rotateY = 0f, rotateZ = 0f;
+    float positionX = 0.0f, positionY = 0.0f, positionZ = -7.0f;
 
     public void RenderInit() {
 
@@ -14,88 +15,95 @@ public class Render extends RenderMain {
 
     public void KeyCheck() {
 
-        if (Keyboard.isKeyDown(Keyboard.KEY_Q)) {
-            rotateZ -= 1f;
-        }
-
-        if (Keyboard.isKeyDown(Keyboard.KEY_E)) {
-            rotateZ += 1f;
-
+        if (Keyboard.isKeyDown(Keyboard.KEY_SPACE)) {
+            //rotateZ -= 1f;
+            positionY -= 0.1f;
         }
 
         if (Keyboard.isKeyDown(Keyboard.KEY_A)) {
-            rotateY -= 1f;
+            //rotateY -= 1f;
+            positionX += 0.1f;
         }
 
         if (Keyboard.isKeyDown(Keyboard.KEY_D)) {
-            rotateY += 1f;
+            //rotateY += 1f;
+            positionX -= 0.1f;
 
         }
 
         if (Keyboard.isKeyDown(Keyboard.KEY_W)) {
-            rotateX -= 1f;
-
+            //rotateX -= 1f;
+            positionZ += 0.1f;
         }
 
         if (Keyboard.isKeyDown(Keyboard.KEY_S)) {
-            rotateX += 1f;
-
+            //rotateX += 1f;
+            positionZ -= 0.1f;
         }
-
     }
 
     public void RenderLoop() {
 
         super.RenderLoop();
 
-        GL11.glTranslatef(0f,0.0f,-7f);
+        GL11.glTranslatef(positionX,positionY,positionZ);
         GL11.glRotatef(rotateX,1.0f,0.0f,0.0f);
         GL11.glRotatef(rotateY,0.0f,1.0f,0.0f);
         GL11.glRotatef(rotateZ,0.0f,0.0f,1.0f);
+
+        GL11.glColor3f( 1.0f, 1.0f, 1.0f);
+
+        GL11.glBegin(GL11.GL_QUADS);
+        GL11.glVertex3f(-100.0f,-1.0f, 100.0f);
+        GL11.glVertex3f( 100.0f,-1.0f, 100.0f);
+        GL11.glVertex3f( 100.0f,-1.0f,-100.0f);
+        GL11.glVertex3f(-100.0f,-1.0f,-100.0f);
+        GL11.glEnd();
+
 
         GL11.glBegin(GL11.GL_QUADS);
 
         GL11.glColor3f( 0.0f, 1.0f, 0.0f);
 
         //Top Face
-        GL11.glVertex3f(-1.0f, 1.0f, 1.0f);
-        GL11.glVertex3f( 1.0f, 1.0f, 1.0f);
-        GL11.glVertex3f( 1.0f, 1.0f,-1.0f);
-        GL11.glVertex3f(-1.0f, 1.0f,-1.0f);
+        GL11.glVertex3f( 0.0f, 2.0f, 2.0f);
+        GL11.glVertex3f( 2.0f, 2.0f, 2.0f);
+        GL11.glVertex3f( 2.0f, 2.0f, 0.0f);
+        GL11.glVertex3f( 0.0f, 2.0f, 0.0f);
 
         //Bottom Face
-        GL11.glVertex3f(-1.0f,-1.0f, 1.0f);
-        GL11.glVertex3f( 1.0f,-1.0f, 1.0f);
-        GL11.glVertex3f( 1.0f,-1.0f,-1.0f);
-        GL11.glVertex3f(-1.0f,-1.0f,-1.0f);
+        GL11.glVertex3f( 0.0f, 0.0f, 2.0f);
+        GL11.glVertex3f( 2.0f, 0.0f, 2.0f);
+        GL11.glVertex3f( 2.0f, 0.0f, 0.0f);
+        GL11.glVertex3f( 0.0f, 0.0f, 0.0f);
 
         GL11.glColor3f( 1.0f, 0.0f, 0.0f);
 
         //Left Face
-        GL11.glVertex3f(-1.0f, 1.0f, 1.0f);
-        GL11.glVertex3f(-1.0f,-1.0f, 1.0f);
-        GL11.glVertex3f(-1.0f,-1.0f,-1.0f);
-        GL11.glVertex3f(-1.0f, 1.0f,-1.0f);
+        GL11.glVertex3f( 0.0f, 2.0f, 2.0f);
+        GL11.glVertex3f( 0.0f, 0.0f, 2.0f);
+        GL11.glVertex3f( 0.0f, 0.0f, 0.0f);
+        GL11.glVertex3f( 0.0f, 2.0f, 0.0f);
 
         //Right Face
-        GL11.glVertex3f( 1.0f, 1.0f, 1.0f);
-        GL11.glVertex3f( 1.0f,-1.0f, 1.0f);
-        GL11.glVertex3f( 1.0f,-1.0f,-1.0f);
-        GL11.glVertex3f( 1.0f, 1.0f,-1.0f);
+        GL11.glVertex3f( 2.0f, 2.0f, 2.0f);
+        GL11.glVertex3f( 2.0f, 0.0f, 2.0f);
+        GL11.glVertex3f( 2.0f, 0.0f, 0.0f);
+        GL11.glVertex3f( 2.0f, 2.0f, 0.0f);
 
         GL11.glColor3f( 0.0f, 0.0f, 1.0f);
 
         //Front Face
-        GL11.glVertex3f( 1.0f, 1.0f, 1.0f);
-        GL11.glVertex3f( 1.0f,-1.0f, 1.0f);
-        GL11.glVertex3f(-1.0f,-1.0f, 1.0f);
-        GL11.glVertex3f(-1.0f, 1.0f, 1.0f);
+        GL11.glVertex3f( 2.0f, 2.0f, 2.0f);
+        GL11.glVertex3f( 2.0f, 0.0f, 2.0f);
+        GL11.glVertex3f( 0.0f, 0.0f, 2.0f);
+        GL11.glVertex3f( 0.0f, 2.0f, 2.0f);
 
         //Back Face
-        GL11.glVertex3f( 1.0f, 1.0f,-1.0f);
-        GL11.glVertex3f( 1.0f,-1.0f,-1.0f);
-        GL11.glVertex3f(-1.0f,-1.0f,-1.0f);
-        GL11.glVertex3f(-1.0f, 1.0f,-1.0f);
+        GL11.glVertex3f( 2.0f, 2.0f, 0.0f);
+        GL11.glVertex3f( 2.0f, 0.0f, 0.0f);
+        GL11.glVertex3f( 0.0f, 0.0f, 0.0f);
+        GL11.glVertex3f( 0.0f, 2.0f, 0.0f);
 
         GL11.glEnd();
 
