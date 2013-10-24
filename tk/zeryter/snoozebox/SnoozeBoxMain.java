b8cd8bebe26ec3ccb3a034f0369095af561214b7
Owen Holloway <owen.holloway101@gmail.com>
2013-10-24 22:09:49 +1100

    Removed the control package

--- tk/zeryter/snoozebox/SnoozeBoxMain.java
+++ tk/zeryter/snoozebox/SnoozeBoxMain.java
@@ -2,7 +2,6 @@ package tk.zeryter.snoozebox;
 
 //Owen Holloway [zeryter] ZerytSoft 2013
 
-import tk.zeryter.snoozebox.control.MouseMain;
 import tk.zeryter.snoozebox.render.*;
 
 public class SnoozeBoxMain {
@@ -10,7 +9,6 @@ public class SnoozeBoxMain {
     public static void main(String[] args) {
 
        new Thread(Variables.renderMain).start();
-       //new Thread(Variables.mouseMain).start();
 
     }
 
@@ -24,7 +22,6 @@ public class SnoozeBoxMain {
 
         //Thread Main Classes (static so that they can be access globally)
         public static Render renderMain = new Render();
-        public static MouseMain mouseMain = new MouseMain();
 
     }
 
